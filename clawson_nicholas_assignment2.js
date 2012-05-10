5624561291d773b3747faf34a0ab6d2ecfdf2042
Nick Clawson <NickClawson@fullsail.edu>
2012-05-09 22:27:09 -0400

    variables declared and assigned value.

--- clawson_nicholas_assignment2.js
+++ clawson_nicholas_assignment2.js
@@ -1,1 +1,14 @@
+//Author: Nick Clawson
+//Date 5/9/12
+//Assignment: Deliverable 2
+
+//Variable Declaration and Assignment
+var jediMaster = 'Rai Chell',
+	sithPrisoners = 1,
+	sithTargets = ['Teenal','Jax','Trick','Fanad','N\'ix']
+;
+
+//Procedure
+
+
 alert("JavaScript works!");
